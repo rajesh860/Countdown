21223fc450034fc8cbf753d4a82307f5cfb9c70e
Rajesh <rr396312@gmail.com>
2022-10-14 16:34:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var hour = 0;
+var min = 0;
+var sec = 0;
+setInterval(function(){
+  
+  sec++
+  document.getElementById("sec-box").innerHTML = sec
+  document.getElementById("min-box").innerHTML= min
+  document.getElementById("hr-box").innerHTML = hour
+  if (sec >= 60) {
+    sec = 0
+    min++
+    if(min >= 60){
+        sec = 0
+        min = 0
+        hour++
+        if(hour >= 24){
+            sec = 0
+            min = 0
+            hour = 0
+        }
+    }
+  }
+}, 1000);
+        
